c2a5a03cb179d005043e474ef97728d569d5a1e2
Maksim Varkus <mvarkus.w@gmail.com>
2021-04-10 22:13:18 +0100

    Add methods supporting method selection

--- app/controllers/ImageController.js
+++ app/controllers/ImageController.js
@@ -11,9 +11,30 @@ class ImageController {
      * @param {Router} router instance
      */
     switchToFileUploadStage(stage, router) {
-        this._service.registerInputEventHandlers(
-            stage.getDomComponents()['button']['control'],
-            router
+        this._service.registerFileUploadEventHandlers(
+            stage, router
         );
+    }
+
+    /**
+     * @param {Stage} stage instance 
+     * @param {Router} router instance
+     */
+    switchToMethodChoosigStage(stage, router) {
+        this._service.registerMethodChoosingEventHandlers(
+            stage, router
+        );
+    }
+
+    retrieveChosenMethodContext() {
+        return this._service.getChosenMethodContext();
+    }
+
+    adjustBrightnessBootstrap(stage, router) {
+        console.log(stage);
+    }
+
+    adjustContrastBootstrap(stage, router) {
+        console.log(stage);
     }
 }
