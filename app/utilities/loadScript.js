bd06ae12c9a9237a70a4ba26003b7cf04679e94c
Maksim Varkus <mvarkus.w@gmail.com>
2021-04-12 21:21:12 +0100

    Turn off async from script and turn on defer

--- app/utilities/loadScript.js
+++ app/utilities/loadScript.js
@@ -2,6 +2,8 @@ const loadScript = (name) => {
     return new Promise((resolve, reject) => {
         const script = document.createElement('script');
         script.src = name;
+        script.async = false;
+        script.defer = true;
         document.body.append(script);
 
         script.onload = () => resolve();
